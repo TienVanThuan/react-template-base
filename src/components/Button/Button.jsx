ddcfd921cba00abc9fede01dbccc4464c9f7df6d
TienVanThuan <tienvanthuan.dev@gmail.com>
2025-11-13 22:40:54 +0700

    init template

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React from 'react';
+import styles from './Button.module.scss';
 
-const Button = () => {
-  return <div>Button</div>;
-};
-
-export default Button;
+export default function Button({ children }) {
+  return (
+    <button className={`${styles.btn} inline-flex items-center gap-2 px-4 py-2 rounded-md`}>
+      {children}
+    </button>
+  );
+}
